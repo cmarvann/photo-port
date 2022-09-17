a7a0a864a34fb8fa280f486b4bdf2f0bf77bf268
cmarvann <covingtonmarvann@gmail.com>
2022-09-17 16:18:27 -0400

    About basis

---
+++ photos/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/about';
+
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    <Nav></Nav>
+      <main>
+       
+        <About></About>
+        
+      </main>
+
+    </div>
+
+  );
+}
+
+export default App;
